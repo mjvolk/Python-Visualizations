088dc700fdf96d1c01649b06375938b0f481bdfb
Michael Volk <Michael@ip-128-239-135-106.v4.wm.edu>
2015-07-20 11:56:29 -0400

    added comments

--- donor-map-gen.py
+++ donor-map-gen.py
@@ -1,28 +1,25 @@
 import pandas as pd
 import json
 import vincent
-from vincent.legends import LegendProperties
-from vincent.properties import PropertySet
-from vincent.values import ValueRef
 import urllib2
 from progressbar import ProgressBar
 import os
 import sys
 
-# with open('http://raw.githubusercontent.com/wrobstory/vincent_map_data/master/world-countries.topo.json', 'r') as f:
-#     get_id = json.load(f)
-
+# Gets project data from AidData project api for an organization indicated by its AidData api id
 def getProjectData(index, organization, years):
     url = 'http://api.aiddata.org/aid/project?size=50&fo=' + str(organization) + '&from=' + str(index) + '&y=' + str(years)
     result = json.load(urllib2.urlopen(url))
     return result
 
+# Adds countries that have not received donations to the list so that they can be displayed on the map
 def addNonDonatedCountries(donor_dict, world):
     for country in world['objects']['world-countries']['geometries']:
         if country['id'] not in donor_dict:
             donor_dict[country['id']] = 0.0
     return donor_dict
 
+# Creates a string of years to search in the api
 def getYearString(start_year, finish_year):
     result = ''
     while start_year < finish_year:
@@ -35,6 +32,7 @@ def getYearString(start_year, finish_year):
 request = urllib2.urlopen('https://raw.githubusercontent.com/wrobstory/vincent_map_data/master/world-countries.topo.json')
 get_id = json.load(request)
 
+# create a dataframe of iso-3 codes in for use in builiding the map
 geometries = get_id['objects']['world-countries']['geometries']
 iso_codes = [x['id'] for x in geometries]
 country_df = pd.DataFrame({'iso_a3': iso_codes}, dtype=str)
@@ -48,16 +46,16 @@ start_year = int(sys.argv[2])
 end_year = int(sys.argv[3])
 
 # Used for testing
-# organization_id = 6
+# organization_id = 28
 # start_year = 2004
 # end_year = 2013
-
 
 url = 'http://api.aiddata.org/aid/project?size=50&fo=' + str(organization_id)
 organizations_url = 'http://api.aiddata.org/data/origin/organizations?'
 json_orgs = json.load(urllib2.urlopen(organizations_url))
 donating_org = ''
 
+# Finds the donating organization based on the id
 for org in json_orgs['hits']:
     if org['id'] == organization_id:
         donating_org = org['name']
@@ -72,12 +70,14 @@ count = 0
 country_dict = {}
 pbar = ProgressBar(maxval=num_projects).start()
 
-# print str(num_projects)
+# Iterates over the projects from the AidData api in chunks of 50, the max size allowed by the api
 while (count < num_projects):
     project_info = getProjectData(count, organization_id, year_range)
     for project in project_info['items']:
+        # Only looks at projects that have transaction values
         if 'transactions' in project:
             for transactions in project['transactions']:
+                # Ignores projects that don't indicate a recipient country
                 if 'tr_receiver_country' in transactions and transactions['tr_receiver_country']['iso3'] != '':
                     donor = transactions['tr_funding_org']['name']
                     receiver = transactions['tr_receiver_country']['iso3']
@@ -95,7 +95,6 @@ while (count < num_projects):
         to_add = num_projects - (count - 50)
         pbar.update(to_add)
 
-# print country_dict[donating_org]
 pbar.finish()
 
 geo_data = [{'name': 'countries',
@@ -108,6 +107,7 @@ receiving_df = pd.DataFrame(list(country_dict[donating_org].iteritems()), column
 
 merged = pd.merge(receiving_df, country_df, on='iso_a3', how='inner')
 
+# Uses vincent to create a map in vega format
 vis = vincent.Map(data=merged, geo_data=geo_data, scale=1000, projection='patterson',
           data_bind='total_received', data_key='iso_a3',
           map_key={'countries': 'id'})
@@ -116,5 +116,6 @@ vis.legend(title="Donation Amount")
 json_file_name = donating_org.replace(' ', '_') + '_donations.json'
 png_file_name = donating_org.replace(' ', '_') + '_donations.png'
 vis.to_json(json_file_name)
+# Transforms the vega json into a donor map image using the vg2png command line function
 os.system("vg2png " + json_file_name + " " + png_file_name)
 
