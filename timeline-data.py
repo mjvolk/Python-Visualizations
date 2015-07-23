9f90af95777f5c2326fc04665136f0ae4131eb92
Michael Volk <michaelvolk@utexas.edu>
2015-07-23 13:01:59 -0400

    Added timeout protection for api calls

--- timeline-data.py
+++ timeline-data.py
@@ -1,11 +1,15 @@
 import urllib2
 import json
 import pandas as pd
-import progressbar as pbar
+import socket
+from progressbar import ProgressBar
 
+
+timeout = 4
+socket.setdefaulttimeout(timeout)
 
 def getDonorData(donor, yearlist, index):
-    url = 'http://api.aiddata.org/aid/project?fo=' + str(donor) + '&size=50&y=' + yearlist + '&from=' + str(index)
+    url = 'http://api.aiddata.org/aid/project?fo=' + str(donor) + '&size=50&y=' + yearlist + '&from=' + str(index) + '&src=1,2,3,4,5,6,7,3249668'
     result = json.load(urllib2.urlopen(url))
     return result
 
@@ -18,56 +22,83 @@ def getYearString(start_year, finish_year):
     result += str(finish_year)
     return result
 
-organization_id = 6
-start_year = 2000
-end_year = 2010
+def getStartYears(year_list):
+    result = list()
+    current_start = year_list[0]
+    previous_year = year_list[0]
+    for year in year_list:
+        if year != year_list[0]:
+            if year == previous_year+1:
+                previous_year = year
+            else:
+                result.append(current_start)
+                current_start = year
+                previous_year = year
+    result.append(current_start)
+    return result
+
+def getEndYears(year_list):
+    result = list()
+    current_end = year_list[0]
+    previous_year = year_list[0]
+    for year in year_list:
+        if year != year_list[0]:
+            if year == previous_year+1:
+                previous_year = year
+                current_end = year
+            else:
+                result.append(current_end)
+                current_end = year
+                previous_year = year
+    result.append(year_list[len(year_list)-1])
+    return result
+
+start_year = 1900
+end_year = 2020
 yearlist = getYearString(start_year, end_year)
+json_orgs = json.load(urllib2.urlopen('http://api.aiddata.org/data/origin/organizations?'))
+index = 0
 
-num_projects = getDonorData(organization_id, yearlist, 0)['project_count']
-count = 0
-year_dict = {}
-
-while(count < num_projects):
-    source_info = getDonorData(organization_id, yearlist, count)
-    for source in source_info['items']:
-        if 'transactions' in source and int(source['transactions'][0]['tr_year']) >= start_year:
-            if int(source['transactions'][0]['tr_year']) not in year_dict:
-                year_dict[int(source['transactions'][0]['tr_year'])] = list()
-            if source['source']['label'] not in year_dict[int(source['transactions'][0]['tr_year'])]:
-                year_dict[int(source['transactions'][0]['tr_year'])].append(source['source']['label'])
-    count += 50
-
-print year_dict
-max_sources = 0
-for year in year_dict:
-    if len(year_dict[year]) > max_sources:
-        max_sources = len(year_dict[year])
-for year in year_dict:
-    if len(year_dict[year]) < max_sources:
-        while len(year_dict[year]) < max_sources:
-            year_dict[year].append(None)
-dates_and_sources = pd.DataFrame(year_dict)
-dates_and_sources.to_csv('dates_and_sources.csv')
-# dates_and_sources.to_csv('dates_and_sources.csv')
-
-# year_dict = {}
-# index = 0
-# for year in dates_and_sources['Years']:
-#     if year not in year_dict:
-#         year_dict[year] = list()
-#     if dates_and_sources['Sources'][index] not in year_dict[year]:
-#         year_dict[year].append(dates_and_sources['Sources'][index])
-#     index += 1
+for org in json_orgs['hits']:
+    organization_id = org['id']
+    donating_org = org['name']
+    print 'Creating Timeline for ' + donating_org
+    num_projects = getDonorData(organization_id, yearlist, 0)['project_count']
+    print 'Processing ' + str(num_projects) + ' projects'
+    count = 0
+    year_dict = {}
 
 
+    while(count < num_projects):
+        while (True):
+            try:
+                source_info = getDonorData(organization_id, yearlist, count)
+                break
+            except socket.timeout:
+                print 'Timeout: retrying API call'
+        for source in source_info['items']:
+            if 'transactions' in source and int(source['transactions'][0]['tr_year']) >= start_year:
+                year = int(source['transactions'][0]['tr_year'])
+                current_source = source['source']['label']
+                if current_source not in year_dict:
+                    year_dict[current_source] = list()
+                if year not in year_dict[current_source]:
+                    year_dict[current_source].append(year)
+                    year_dict[current_source].sort()
+        count += 50
+        print str(count)
 
 
-# dates_and_sources = list()
-# source = [datetime(2012, 1, 1), 'OECD']
-# source2 = [datetime(2011, 1, 1), 'OECD']
-# dates_and_sources.append(source)
-# dates_and_sources.append(source2)
-# dates_and_sources.sort()
-# # print dates_and_sources
-# df = pd.DataFrame(dates_and_sources, columns=['Date', 'Source'])
-# print df
+    # print year_dict
+    timeline_dict = {}
+    for source in year_dict:
+        timeline_dict[source] = {}
+        timeline_dict[source]['Start'] = getStartYears(year_dict[source])
+        timeline_dict[source]['End'] = getEndYears(year_dict[source])
+
+
+    dates_and_sources = pd.DataFrame(timeline_dict)
+    file_name = 'src_timelines/' + donating_org.replace(' ', '_') + '_source_timeline.json'
+    dates_and_sources.to_json(file_name)
+    index += 1
+    print str(index) + ' out of 100 completed'
