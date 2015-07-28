4eb9d4efa4e24b295cc5a00ca51b35269b6d3120
mjvolk <michaelvolk@utexas.edu>
2015-07-28 11:59:17 -0400

    chart headers added

--- create-html.py
+++ create-html.py
@@ -2,8 +2,10 @@ import sys
 
 source = sys.argv[1]
 file_name = sys.argv[2]
+source_name = sys.argv[3]
 html = """<html>
   <head>
+    <h2 align = "center">""" + source_name + """</h2>
     <script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['timeline']}]}"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script type="text/javascript">
@@ -37,14 +39,13 @@ function drawChart(json) {
 
     for (source in json) {
         for (i = 0; i < json[source]['Start'].length; i++) {
-            dataTable.addRow([source, new Date(json[source]['Start'][i], 0, 1), new Date(json[source]['End'][i], 11, 31)]);
+            dataTable.addRow([source, new Date(json[source]['Start'][i], 0, 1), new Date(json[source]['End'][i] + 1, 0, 1)]);
         }
     }
 
     var options = {
-        'title': 'Austria Source Information',
             'width': 1000,
-            'height': 1000,
+            'height': 1000
     };
 
     chart.draw(dataTable, options);
