931014a0f5555ce727201f88fa42c8c5c45c163a
mjvolk <michaelvolk@utexas.edu>
2015-08-07 16:10:04 -0400

    added further comments to donor map script

--- donor-map-gen.py
+++ donor-map-gen.py
@@ -2,7 +2,6 @@ import pandas as pd
 import json
 import vincent
 import urllib2
-from progressbar import ProgressBar
 import socket
 import os
 import sys
@@ -39,6 +38,7 @@ def getYearString(start_year, finish_year):
     result += str(finish_year)
     return result
 
+# A base map to be used by the map creator
 request = urllib2.urlopen('https://raw.githubusercontent.com/mjvolk/Python-Visualizations/master/world-countries.topo.json')
 get_id = json.load(request)
 
@@ -49,17 +49,18 @@ country_df = pd.DataFrame({'iso_a3': iso_codes}, dtype=str)
 
 # First cmd line parameter is the organization id as specified by the organization id numbers on
 # the AidData API
-# organization_id = int(sys.argv[1])
-# # Second cmd line parameter is the start year for the project data
-# start_year = int(sys.argv[2])
-# # Third cmd line parameter is the end year for the project data
-# end_year = int(sys.argv[3])
+organization_id = int(sys.argv[1])
+# Second cmd line parameter is the start year for the project data
+start_year = int(sys.argv[2])
+# Third cmd line parameter is the end year for the project data
+end_year = int(sys.argv[3])
 
 # Used for testing
-organization_id = 28
-start_year = 2003
-end_year = 2013
+# organization_id = 28
+# start_year = 2003
+# end_year = 2016
 
+# Retrieves list of donor organizations and their IDs from AidData API
 organizations_url = 'http://api.aiddata.org/data/origin/organizations?'
 json_orgs = json.load(urllib2.urlopen(organizations_url))
 donating_org = ''
@@ -72,12 +73,13 @@ for org in json_orgs['hits']:
 
 print 'Creating map for ' + donating_org
 
+# Gets total number of projects for a donor in the given time frame
 json_result = getProjectData(0, organization_id, getYearString(start_year, end_year))
 num_projects = json_result['project_count']
+print 'Evaluating ' + str(num_projects) + ' projects'
 year_range = getYearString(start_year, end_year)
 count = 0
 country_dict = {}
-pbar = ProgressBar(maxval=num_projects).start()
 
 # Iterates over the projects from the AidData api in chunks of 50, the max size allowed by the api
 while (count < num_projects):
@@ -91,29 +93,31 @@ while (count < num_projects):
                     donor = transactions['tr_funding_org']['name']
                     receiver = transactions['tr_receiver_country']['iso3']
                     amount = transactions['tr_constant_value']
+                    # Adds donor to dictionary if it isn't there yet
                     if donor not in country_dict:
                         country_dict[donor] = {}
+                    # Adds receiver to donors dictionary with amount if it has not appeared yet
                     if receiver not in country_dict[donor]:
                         country_dict[donor][receiver] = amount
+                    # If the donor and receiver already exist, the receiver has the new project cost added
+                    # to it's running total
                     else:
                         country_dict[donor][receiver] += amount
     count += 50
-    if(count < num_projects):
-        pbar.update(count)
-    else:
-        to_add = num_projects - (count - 50)
-        pbar.update(to_add)
+    print '{}/{}\r'.format(count, num_projects),
 
-pbar.finish()
-
+# Creates geo-data outline for vincent/vega operations
 geo_data = [{'name': 'countries',
-             'url': 'https://raw.githubusercontent.com/wrobstory/vincent_map_data/master/world-countries.topo.json',
+             'url': 'https://raw.githubusercontent.com/mjvolk/Python-Visualizations/master/world-countries.topo.json',
              'feature': 'world-countries'}]
 
+# Adds countries to dictionary that were not donated to so they still appear on map
 country_dict[donating_org] = addNonDonatedCountries(country_dict[donating_org], get_id)
 
+# A dataframe of all iso3 codes with their associated recipient totals
 receiving_df = pd.DataFrame(list(country_dict[donating_org].iteritems()), columns=['iso_a3', 'total_received'])
 
+# Merges receiving_df with country_df for use with vincent/vega
 merged = pd.merge(receiving_df, country_df, on='iso_a3', how='inner')
 
 # Uses vincent to create a map in vega format
@@ -127,5 +131,6 @@ json_file_name = donating_org.replace(' ', '_') + '_donations.json'
 png_file_name = donating_org.replace(' ', '_') + '_donations.png'
 vis.to_json(json_file_name)
 # Transforms the vega json into a donor map image using the vg2png command line function
+# Must have vega installed to work
 os.system("vg2png " + json_file_name + " " + png_file_name)
 
